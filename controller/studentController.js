324f84138c3ade0067329c8c74f52e24be1c0175
Shubham Kadam <shubham.kadam@appgambit.com>
2026-03-14 15:39:59 +0530

    add route controller structure and create,delete,update api for students

---
+++ controller/studentController.js
@@ -0,0 +1,68 @@
+const dbConnection = require("../utils/db-connection");
+
+const addEntries = (req, res) => {
+  const { name, email, password } = req.body;
+  const insertQuery = `INSERT INTO students (name, email, password) VALUES (?, ?, ?)`;
+
+  dbConnection.execute(insertQuery, [name, email, password], (err) => {
+    if (err) {
+      console.log("error while inserting", err);
+      res.status(500).json({ message: "error while inserting" });
+      dbConnection.end();
+      return;
+    }
+    res.status(200).json({ message: `inserted ${name} and ${email}` });
+  });
+};
+
+const updateEntries = (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  const updateQuery = `UPDATE students SET name = ? , email = ? WHERE id = ?`;
+
+  dbConnection.execute(updateQuery, [name, email, id], (err, result) => {
+    if (err) {
+      console.log("error while updating", err);
+      res.status(500).json({ message: "error while updating" });
+      dbConnection.end();
+      return;
+    }
+
+    if (result.affectedRows === 0) {
+      res.status(404).json({ message: `no student found with id ${id}` });
+      dbConnection.end();
+      return;
+    }
+
+    res.status(200).json({ message: `student updated successfully` });
+  });
+};
+
+const deleteEntries = (req, res) => {
+  const { id } = req.params;
+  const deleteQuery = `DELETE FROM students WHERE id = ?`;
+
+  dbConnection.execute(deleteQuery, [id], (err, result) => {
+    if (err) {
+      console.log("error while deleting", err);
+      res.status(500).json({ message: "error while  deleteing" });
+      dbConnection.end();
+      return;
+    }
+
+    if (result.affectedRows === 0) {
+      res.status(404).json({ message: `no student found with id ${id}` });
+      dbConnection.end();
+      return;
+    }
+    res
+      .status(200)
+      .json({ message: `student with id ${id} deleted successfully` });
+  });
+};
+
+module.exports = {
+  addEntries,
+  updateEntries,
+  deleteEntries,
+};
--- index.js
+++ index.js
@@ -1,64 +1,10 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2");
+const db = require("./utils/db-connection");
+const studentRoute = require("./routes/studentsRoute");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "testDB",
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log("Connected to MySQL");
-
-  const userTableQuery = `CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-  )`;
-
-  const busesTableQuery = `CREATE TABLE IF NOT EXISTS buses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    busNumber INT NOT NULL,
-    totalSeats INT NOT NULL,
-    availableSeats INT NOT NULL,
-  )`;
-
-  const bookingTableQuery = `CREATE TABLE IF NOT EXISTS bookings (
-    id INT PRIMARY KEY,
-    seatNumber INT NOT NULL,  
-  )`;
-
-  const payementsTableQuery = `CREATE TABLE IF NOT EXISTS payements (
-    id INT PRIMARY KEY,
-    amountPaid INT NOT NULL,
-    paymentStatus VARCHAR(255) NOT NULL,
-    
-  )`;
-
-  const tables = [
-    userTableQuery,
-    busesTableQuery,
-    bookingTableQuery,
-    payementsTableQuery,
-  ];
-
-  tables.forEach((tables) => {
-    connection.execute(tables, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(result);
-    });
-  });
-});
+app.use(express.json());
+app.use("/students", studentRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
